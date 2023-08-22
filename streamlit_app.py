81f4884061443c4f86176dcbbdbca0fa849da3df
mesfinsekru23 <142558518+mesfinsekru23@users.noreply.github.com>
2023-08-22 10:49:14 -0400

    Update streamlit_app.py
    added secrets

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,9 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
