d8eaec2e24f87316d9076d215f5b3870c8ede89b
mesfinsekru23 <142558518+mesfinsekru23@users.noreply.github.com>
2023-08-22 17:01:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,4 @@
-import streamlit
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Brekfasts Favorites')
-streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-
 import snowflake.connector
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
